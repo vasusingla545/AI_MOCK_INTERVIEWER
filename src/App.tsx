0d4a2283143e2dc3c466c78b445e8597e9c84ae0
vasusingla545 <iec2022046@iiita.ac.in>
2025-04-30 17:01:27 +0530

    Changes: Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { Button } from './components/ui/button'
-import { DESTRUCTION } from 'dns'
-
+// import React from 'react'
+// import { Button } from './components/ui/button'
+// import { DESTRUCTION } from 'dns'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PublicLayout from '@/layouts/public-layout'
+import HomePage from '@/routes/home'
 const App = () => {
   return (
-    <div className='text-blue-500'><Button variant={"destructive"} size={"icon"}>Click here</Button></div>
+    
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+      <Route element={<PublicLayout/>}>
+      <Route index element={<HomePage/>}>
+      {/* <Route element={<PublicLayout/>}> */}
+      
+      
+      </Route>
+      </Route>
+
+
+        {/* Protected Routes */}
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/layouts/public-layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/footer"
+import Header from "@/components/header"
+import { Outlet } from "react-router-dom"
+
+
+const PublicLayout = () => {
+    return (
+        <div className="w-full">public-layout
+         {/* handler to store the user data (if the user is looged in or logged out like such in Firebase) */}
+        <Header/>
+        <Outlet/>
+        <Footer/>
+        </div>
+        
+    )
+}
+
+export default PublicLayout
